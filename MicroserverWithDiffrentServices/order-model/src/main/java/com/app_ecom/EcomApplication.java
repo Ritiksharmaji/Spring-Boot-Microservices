a3d2f1ae5cef943f297918004f441f9f77626069
Ritiksharmaji <ritiksharma555598@gmail.com>
2025-12-28 16:13:57 +0530

    fixing problem to use postgresql inplace of mysql

--- MicroserverWithDiffrentServices/order-model/src/main/java/com/app_ecom/EcomApplication.java
+++ MicroserverWithDiffrentServices/order-model/src/main/java/com/app_ecom/EcomApplication.java
@@ -2,8 +2,13 @@ package com.app_ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.app_ecom.repository")
+@EntityScan(basePackages = "com.app_ecom.model")
+
 public class EcomApplication {
 
 	public static void main(String[] args) {
